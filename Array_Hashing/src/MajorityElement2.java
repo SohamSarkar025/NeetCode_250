8b987e1e52cf7b3126cb8fd98d48930f5d45bc59
Soham Sarkar <srutidhara153@gmail.com>
2025-06-23 13:09:40 +0530

    Majority Element

---
+++ Array_Hashing/src/MajorityElement2.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class MajorityElement2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        System.out.println(list);
+    }
+}
